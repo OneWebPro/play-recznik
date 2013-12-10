eeef6ab92c953820a671f736c9e5e3ca884d2f04
mromanski <MRomanski@wp-sa.pl>
2013-12-10 13:00:14 +0100

    Add global object.

---
+++ app/Global.scala
@@ -0,0 +1,44 @@
+import play.api._
+import play.api.Play.current
+import scala.slick.jdbc.meta.MTable
+import play.api.db.slick.DB
+import play.api.db.slick.Config.driver.simple._
+
+/**
+ * @author loki
+ */
+object Global extends GlobalSettings{
+
+  /**
+   * Database structure
+   */
+  lazy val ddl = dao.DDL.ddl
+
+  /**
+   * If database is not exists we create new.
+   * @param app play.api.Application
+   */
+  override def onStart(app: Application) {
+    DB.withSession {
+      implicit s: scala.slick.session.Session => {
+        if (MTable.getTables().list().isEmpty) {
+          ddl.create
+        }
+      }
+    }
+  }
+
+  /**
+   * We remove database when we are stopping application.
+   * @param app play.api.Application
+   */
+  override def onStop(app: Application) {
+    DB.withSession {
+      implicit s: scala.slick.session.Session =>
+        if (!MTable.getTables().list().isEmpty && !Play.isDev) {
+          ddl.drop
+        }
+    }
+  }
+
+}
