8eccac0ee5f2737826676fded3ae85b165ffb617
mromanski <MRomanski@wp-sa.pl>
2013-12-11 13:35:29 +0100

    Add serbian special letters.

--- app/shared/Letters.scala
+++ app/shared/Letters.scala
@@ -14,4 +14,25 @@ object Letters  extends Enumeration{
    * Š š
    * Ž ž
    **/
+
+  val C = Value(0, "Č")
+
+  val c = Value(1, "č")
+
+  val CC = Value(2, "Ć")
+
+  val cc = Value(3, "ć")
+
+  val D = Value(4, "Đ")
+
+  val d = Value(5, "đ")
+
+  val S = Value(6, "Š")
+
+  val s = Value(7, "š")
+
+  val Z = Value(8, "Ž")
+
+  val z = Value(9, "ž")
+
 }
