f325f68d5d7d2eeada7f069420d2da5a94f669ff
mromanski <MRomanski@wp-sa.pl>
2013-12-13 11:31:54 +0100

    WordsService tests.

--- test/services/WordsServiceSpec.scala
+++ test/services/WordsServiceSpec.scala
@@ -2,6 +2,7 @@ package test.services
 
 
 import org.specs2.mutable._
+import service.WordsService
 
 
 /**
@@ -14,33 +15,57 @@ class WordsServiceSpec extends Specification with GlobalDatabaseTests {
   import dao._
 
   "WordsService.addTranslation" should {
-    "add translation with one new words" in {
+    "add translation with new words" in {
       runSession {
         implicit session =>
+          val polishWord: String = "x"
+          val serbianWord: String = "x2"
+          val translations = WordToWordTable.findAll()
+          val polish = PolishWordTable.findAll()
+          val serbian = SerbianWordTable.findAll()
+          WordsService.addTranslation(Translation(
+            PolishTranslation(None, polishWord),
+            SerbianTranslation(None, serbianWord)
+          )).isLeft mustEqual false
+          WordToWordTable.findAll().size mustEqual (translations.size + 1)
+          PolishWordTable.findAll().size mustEqual (polish.size + 1)
+          SerbianWordTable.findAll().size mustEqual (serbian.size + 1)
       }
     }
   }
 
   "WordsService.addTranslation" should {
-    "add translation with many new words" in {
+    "add translation to polish exists word" in {
       runSession {
         implicit session =>
+          val serbianWord: String = "x2"
+          val default = PolishWordTable.findByLetter("Afryka".toLowerCase).first()
+          val translations = WordToWordTable.findAll()
+          val serbian = SerbianWordTable.findAll()
+          WordsService.addTranslation(Translation(
+            PolishTranslation(default.id, ""),
+            SerbianTranslation(None, serbianWord)
+          )).isLeft mustEqual false
+          WordToWordTable.findAll().size mustEqual (translations.size + 1)
+          SerbianWordTable.findAll().size mustEqual (serbian.size + 1)
       }
     }
   }
 
   "WordsService.addTranslation" should {
-    "add translation to exists word" in {
+    "add translation to serbian exists word" in {
       runSession {
         implicit session =>
-      }
-    }
-  }
-
-  "WordsService.addTranslation" should {
-    "add many translations to exists word" in {
-      runSession {
-        implicit session =>
+          val polishWord: String = "x2"
+          val default = SerbianWordTable.findByLetter("Afrika".toLowerCase).first()
+          val translations = WordToWordTable.findAll()
+          val polish = PolishWordTable.findAll()
+          WordsService.addTranslation(Translation(
+            PolishTranslation(None,polishWord),
+            SerbianTranslation(default.id, "")
+          )).isLeft mustEqual false
+          WordToWordTable.findAll().size mustEqual (translations.size + 1)
+          PolishWordTable.findAll().size mustEqual (polish.size + 1)
       }
     }
   }
@@ -49,6 +74,17 @@ class WordsServiceSpec extends Specification with GlobalDatabaseTests {
     "edit polish translation but only added by user" in {
       runSession {
         implicit session =>
+          val polishWord: String = "x"
+          val serbianWord: String = "x2"
+          val translations = WordsService.addTranslation(Translation(
+            PolishTranslation(None, polishWord),
+            SerbianTranslation(None, serbianWord)
+          )).right.get
+          WordsService.editPolishTranslation(PolishTranslation(translations.polish.id, "zz")).isLeft mustEqual false
+          val changedTranslation = PolishWordTable.findById(translations.polish.id.get).get
+          changedTranslation.word mustEqual "zz"
+          val default = PolishWordTable.findByLetter("Afryka".toLowerCase).first()
+          WordsService.editPolishTranslation(PolishTranslation(default.id, "zz")).isLeft mustEqual true
       }
     }
   }
@@ -57,6 +93,17 @@ class WordsServiceSpec extends Specification with GlobalDatabaseTests {
     "edit serbian translation but only added by user" in {
       runSession {
         implicit session =>
+          val polishWord: String = "x"
+          val serbianWord: String = "x2"
+          val translations = WordsService.addTranslation(Translation(
+            PolishTranslation(None, polishWord),
+            SerbianTranslation(None, serbianWord)
+          )).right.get
+          WordsService.editSerbianTranslation(SerbianTranslation(translations.serbian.id, "zz")).isLeft mustEqual false
+          val changedTranslation = SerbianWordTable.findById(translations.serbian.id.get).get
+          changedTranslation.word mustEqual "zz"
+          val default = SerbianWordTable.findByLetter("Afrika".toLowerCase).first()
+          WordsService.editSerbianTranslation(SerbianTranslation(default.id, "zz")).isLeft mustEqual true
       }
     }
   }
@@ -65,6 +112,17 @@ class WordsServiceSpec extends Specification with GlobalDatabaseTests {
     "remove polish translation but only added by user" in {
       runSession {
         implicit session =>
+          val polishWord: String = "x"
+          val serbianWord: String = "x2"
+          val translations = WordsService.addTranslation(Translation(
+            PolishTranslation(None, polishWord),
+            SerbianTranslation(None, serbianWord)
+          )).right.get
+          WordsService.removePolishTranslation(RemovePolishTranslation(translations.polish.id.get)).isLeft mustEqual false
+          val changedValue = PolishWordTable.findById(translations.polish.id.get)
+          changedValue.isDefined mustNotEqual true
+          val default = PolishWordTable.findByLetter("Afryka".toLowerCase).first()
+          WordsService.removePolishTranslation(RemovePolishTranslation(default.id.get)).isLeft mustEqual true
       }
     }
   }
@@ -73,6 +131,17 @@ class WordsServiceSpec extends Specification with GlobalDatabaseTests {
     "remove serbian translation but only added by user" in {
       runSession {
         implicit session =>
+          val polishWord: String = "x"
+          val serbianWord: String = "x2"
+          val translations = WordsService.addTranslation(Translation(
+            PolishTranslation(None, polishWord),
+            SerbianTranslation(None, serbianWord)
+          )).right.get
+          WordsService.removeSerbianTranslation(RemoveSerbianTranslation(translations.serbian.id.get)).isLeft mustEqual false
+          val changedValue = SerbianWordTable.findById(translations.serbian.id.get)
+          changedValue.isDefined mustNotEqual true
+          val default = SerbianWordTable.findByLetter("Afrika".toLowerCase).first()
+          WordsService.removeSerbianTranslation(RemoveSerbianTranslation(default.id.get)).isLeft mustEqual true
       }
     }
   }
